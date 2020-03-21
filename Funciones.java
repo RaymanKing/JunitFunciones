d4f5db69e086b21ed86b6f275e2e7c53eefc8f18
espe <esperanza.del.junco@adaits.es>
2020-03-21 18:29:28 +0100

    #10 funcion2

---
+++ Funciones.java
@@ -0,0 +1,22 @@
+
+
+public class Funciones {
+
+    //En esta función comprobaremos si un número se encuentra dentro del intervalo (400,500).
+
+
+    public static boolean funcion2 (int numero){
+
+
+    if(numero>=400 && numero <=500){
+   
+        return true;
+
+    }else{
+        return false;
+    
+         
+        }
+
+     }
+}
