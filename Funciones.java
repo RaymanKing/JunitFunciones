5337c841c89fbc80d6683b0b2248ef5e7de2952c
unknown <enrikerapela@gmail.com>
2020-03-21 17:32:04 +0100

    #3 Funcion numero 6

---
+++ Funciones.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Funciones {
+    public static void main(String[] args) {
+
+    }
+
+    public static ArrayList<Double> funcion6(ArrayList<Double> arrayDoubles) {
+        for (int i = 0; i < arrayDoubles.size(); i++) {
+            if (i % 2 == 0) {
+                arrayDoubles.set(i, arrayDoubles.get(i) * 6);
+            } else {
+                arrayDoubles.set(i, Math.pow(arrayDoubles.get(i), 6));
+            }
+        }
+        return arrayDoubles;
+    }
+
+}
