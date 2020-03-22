1edd2ec8b8280de2c85483602a7f97b7d1df9418
unknown <enrikerapela@gmail.com>
2020-03-22 16:12:58 +0100

    #27 Correcion

--- Funciones.java
+++ Funciones.java
@@ -1,17 +1,40 @@
 public class Funciones {
 
-    public static int funcion5(String cadena1) {
-
-        int sumatorio = 0;
+    public static int funcion5(String cadena1, int number) {
 
         for (int i = 0; i < cadena1.length(); i++) {
-            if (cadena1.charAt(i) <= 9) {
-                sumatorio = sumatorio + cadena1.charAt(i);
-            } else if (cadena1.charAt(i) >= 0) {
-                sumatorio = sumatorio + cadena1.charAt(i);
-            }
 
+            if (cadena1.charAt(i) == 48) {
+                number = number + 0;
+            }
+            if (cadena1.charAt(i) == 49) {
+                number = number + 1;
+            }
+            if (cadena1.charAt(i) == 50) {
+                number = number + 2;
+            }
+            if (cadena1.charAt(i) == 51) {
+                number = number + 3;
+            }
+            if (cadena1.charAt(i) == 52) {
+                number = number + 4;
+            }
+            if (cadena1.charAt(i) == 53) {
+                number = number + 5;
+            }
+            if (cadena1.charAt(i) == 54) {
+                number = number + 6;
+            }
+            if (cadena1.charAt(i) == 55) {
+                number = number + 7;
+            }
+            if (cadena1.charAt(i) == 56) {
+                number = number + 8;
+            }
+            if (cadena1.charAt(i) == 57) {
+                number = number + 9;
+            }
         }
-        return sumatorio;
+        return number;
     }
 }
